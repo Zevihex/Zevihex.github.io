1abe69b644a1dd35f2b527275be9764f8806c7f1
Zevihex <OfficialZevihex@gmail.com>
2025-01-22 14:44:02 -0600

    Polish Gamba Game and Phone Layout

--- js/gamba.js
+++ js/gamba.js
@@ -1,15 +1,31 @@
 const gambaButton = document.getElementById("gambaButton");
 const showArray = document.getElementById("gambaArray");
 const currentNumber = document.getElementById("currentNumber");
+const numberPercent = document.getElementById("percent");
+const boxes = document.getElementsByClassName("boxContainer")[0];
+const box = document.getElementsByClassName("box");
+const click = document.getElementById("clicks");
+let clickNum = 0;
+
 let playerArray = [];
+gambaButton.addEventListener("hover", function() {
+  
+})
 gambaButton.addEventListener("click", function() {
   const rngNum = Math.floor(Math.random() * 100) + 1;
   currentNumber.innerHTML = "Current Number: " + rngNum;
-  if(playerArray.includes(rngNum)) {
-    
-  } else {
+  if(!playerArray.includes(rngNum)) {
     playerArray.push(rngNum);
-    playerArray.sort((a, b) => a - b); 
-    showArray.innerHTML = playerArray;
+    numberPercent.innerHTML = playerArray.length + "%";
+    box[rngNum-1].innerHTML = rngNum;
   }
-  });
+  clickNum++;
+  click.innerHTML = clickNum;
+});
+
+for(let i = 0; i < 100; i++) {
+  const newBox = document.createElement("div");
+  newBox.className = "box";
+  newBox.id = i+1;
+  boxes.appendChild(newBox);
+}
